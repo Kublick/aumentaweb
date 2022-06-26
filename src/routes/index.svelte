94a0ab7a1a1342a3d5f83d14e1c72cac837461bf
Max Blancarte <blancarte@gmail.com>
2022-06-25 22:33:59 -0700

    start of webinar landing

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<div class="md:container md:mx-auto mx-4 text-primary mt-12">
+	<h1 class="text-2xl md:text-5xl text-center tracking-wide font-bold">
+		¡ADIOS AL TEMOR A LAS PLATAFORMAS DIGITALES!
+	</h1>
+	<h2 class="text-xl md:text-4xl mt-2 text-center">
+		Con este <span class="text-secondary">Curso Online</span>, cualquier inseguridad que puedas
+		sentir para posicionarte en el mundo digital SE IRÁ… Y a cambio, llegarán las ganancias que
+		tanto estabas esperando.
+	</h2>
+	<div>
+		<div class="flex flex-wrap justify-center">
+			<div class="w-full md:w-1/2 p-4">
+				<img src="/aumenta_pacientes_logo.png" alt="" />
+			</div>
+			<div class="w-full md:w-1/2 p-4">
+				<img src="https://i.imgur.com/XQQXQZb.png" alt="" />
+			</div>
+		</div>
+	</div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				primary: '#2B4570',
+				secondary: '#fbbf0f',
+				secondarylight: '#fbc527',
+				whatsapp: '#35B624',
+				lightgray: '#383838',
+				facebook: '#1877f2'
+			},
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif']
+			},
+			fontSize: {
+				44: '2.75rem',
+				27: '1.6875rem',
+				93: '5.8125rem'
+			},
+			spacing: {
+				100: '25rem'
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
